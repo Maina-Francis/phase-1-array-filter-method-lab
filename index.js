821eb141be951be7291adedf145df738f52509ab
Francis Maina <sebastianfrancix@gmail.com>
2022-08-25 15:31:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(array, name) {
+  return array.filter((str) => str.toLowerCase() === name.toLowerCase());
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function (letters) {
+    if (letters.indexOf(str) === 0) {
+      return true;
+    } else if (letters.indexOf(str) > 0) {
+      return false;
+    }
+  });
+}
+
+function matchName(drivers, str) {
+  return drivers.filter(function (element) {
+    return element.name === str;
+  });
+}
